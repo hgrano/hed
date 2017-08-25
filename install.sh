590f6ce167be7b22beebf950cdf5ca8364682689
Huw Grano <a1647667@student.adelaide.edu.au>
2017-08-25 10:48:59 +0930

    swapped order of module load and python setup

--- install.sh
+++ install.sh
@@ -1,6 +1,18 @@
 make clean
 module purge
 
+# C/C++ modules for HED compilation
+module load Boost/1.61.0-foss-2016b
+module load protobuf/2.6.1-foss-2016b
+module load glog/0.3.3-foss-2016b
+module load gflags/2.1.2-foss-2016b
+module load HDF5/1.8.17-foss-2016b-serial
+module load OpenCV/3.1.0-foss-2016b
+module load LMDB/0.9.18-foss-2016b
+module load LevelDB/1.18-foss-2016b
+module load snappy/1.1.3-foss-2016b
+module load CUDA/8.0.61
+
 # Python setup for HED
 module load Python/2.7.13-foss-2016b
 mkdir -p $FASTDIR/virtualenvs/hed
@@ -22,17 +34,6 @@ pip install python-gflags
 pip install pyyaml
 pip install Pillow
 pip install six
-
-module load Boost/1.61.0-foss-2016b
-module load protobuf/2.6.1-foss-2016b
-module load glog/0.3.3-foss-2016b
-module load gflags/2.1.2-foss-2016b
-module load HDF5/1.8.17-foss-2016b-serial
-module load OpenCV/3.1.0-foss-2016b
-module load LMDB/0.9.18-foss-2016b
-module load LevelDB/1.18-foss-2016b
-module load snappy/1.1.3-foss-2016b
-module load CUDA/8.0.61
 
 make all -j32
 make pycaffe -j32
