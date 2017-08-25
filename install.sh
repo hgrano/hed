ecc75136249df6ca8acb9f357ff4f28c0e3c3e59
Huw Grano <a1647667@student.adelaide.edu.au>
2017-08-25 12:51:16 +0930

    py 2.7.13 -> 2.7.11

--- install.sh
+++ install.sh
@@ -1,14 +1,10 @@
 make clean
 module purge
 
+module load Boost/1.61.0-foss-2016uofa-Python-2.7.11
 # Python setup for HED
-module load Python/2.7.13-foss-2016b
-mkdir -p $FASTDIR/virtualenvs/hed
-source $FASTDIR/virtualenvs/hed/bin/activate # activate env
-for req in $(cat python/requirements.txt); do pip install $req; done
+module load Python/2.7.11-foss-2016uofa
 
-# C/C++ modules for HED compilation
-module load Boost/1.61.0-foss-2016b
 module load protobuf/2.6.1-foss-2016b
 module load glog/0.3.3-foss-2016b
 module load gflags/2.1.2-foss-2016b
@@ -18,6 +14,23 @@ module load LMDB/0.9.18-foss-2016b
 module load LevelDB/1.18-foss-2016b
 module load snappy/1.1.3-foss-2016b
 module load CUDA/8.0.61
+
+
+mkdir -p $FASTDIR/virtualenvs/hed
+source $FASTDIR/virtualenvs/hed/bin/activate # activate env
+for req in $(cat python/requirements.txt); do pip install $req; done
+
+# C/C++ modules for HED compilation
+# module load Boost/1.61.0-foss-2016b # or Boost/1.61.0-foss-2016uofa-Python-2.7.11
+# module load protobuf/2.6.1-foss-2016b
+# module load glog/0.3.3-foss-2016b
+# module load gflags/2.1.2-foss-2016b
+# module load HDF5/1.8.17-foss-2016b-serial
+# module load OpenCV/3.1.0-foss-2016b
+# module load LMDB/0.9.18-foss-2016b
+# module load LevelDB/1.18-foss-2016b
+# module load snappy/1.1.3-foss-2016b
+# module load CUDA/8.0.61
 
 # pip install Cython
 # pip install numpy
