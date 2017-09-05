ec6cc142014ca40b0430f643ec690bf58a51bcb7
hgrano <huw.grano@student.adelaide.edu.au>
2017-09-05 16:18:24 +0930

    Added num_pixels

--- compute_image_mean.py
+++ compute_image_mean.py
@@ -5,17 +5,18 @@ import argparse
 
 def compute_image_mean(paths_lst):
     rgb_sum = np.array([0.0, 0.0, 0.0])
-    num_imgs = 0
+    num_pixels = 0
     for line in paths_lst:
         path = line.split()[0]
         img = scipy.ndimage.imread(path)
-        if len(img.shape) != 3:
+        shape = img.shape
+        if len(shape) != 3:
             print 'Warning', path, 'is not RGB'
         else:
-            num_imgs += 1
+            num_pxiels += shape[0] * shape[1]
             for channel in range(0, 3):
                 rgb_sum[channel] += np.sum(img[:, :, channel])
-    return rgb_sum / float(num_imgs)
+    return rgb_sum / float(num_pixels)
 
 
 def main(path_to_train_lst):
