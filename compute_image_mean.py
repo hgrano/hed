b478289de86a869b1304004a626ad4210cada188
hgrano <huw.grano@student.adelaide.edu.au>
2017-09-05 16:13:29 +0930

    Added check for RGB

--- compute_image_mean.py
+++ compute_image_mean.py
@@ -5,17 +5,17 @@ import argparse
 
 def compute_image_mean(paths_lst):
     rgb_sum = np.array([0.0, 0.0, 0.0])
+    num_imgs = 0
     for line in paths_lst:
         path = line.split()[0]
         img = scipy.ndimage.imread(path)
         if len(img.shape) != 3:
-            print 'Error! ', 
-            print 'Line =', line
-            print 'Path =', path
-            return 'Error'
-        for channel in range(0, 3):
-            rgb_sum[channel] += np.sum(img[:, :, channel])
-    return rgb_sum / float(len(paths_lst))
+            print 'Warning', path, 'is not RGB'
+        else:
+            num_imgs += 1
+            for channel in range(0, 3):
+                rgb_sum[channel] += np.sum(img[:, :, channel])
+    return rgb_sum / float(num_imgs)
 
 
 def main(path_to_train_lst):
