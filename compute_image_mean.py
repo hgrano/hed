e65f24bd8177ad8960525ebcfe657c86d390354c
hgrano <huw.grano@student.adelaide.edu.au>
2017-09-05 16:19:19 +0930

    Fix typo

--- compute_image_mean.py
+++ compute_image_mean.py
@@ -13,7 +13,7 @@ def compute_image_mean(paths_lst):
         if len(shape) != 3:
             print 'Warning', path, 'is not RGB'
         else:
-            num_pxiels += shape[0] * shape[1]
+            num_pixels += shape[0] * shape[1]
             for channel in range(0, 3):
                 rgb_sum[channel] += np.sum(img[:, :, channel])
     return rgb_sum / float(num_pixels)
