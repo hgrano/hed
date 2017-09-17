adb8efdf7c751ac4ce0653aa56149474b1910b0f
hgrano <huw.grano@student.adelaide.edu.au>
2017-09-17 15:25:18 +0930

    Added main function

--- examples/hed/solve.py
+++ examples/hed/solve.py
@@ -8,8 +8,6 @@ import time
 import os
 import glob
 
-max_time_seconds = float(os.environ['TIMEOUT_SECONDS'])
-start_time = time.time()
 # make a bilinear interpolation kernel
 # credit @longjon
 def upsample_filt(size):
@@ -36,48 +34,54 @@ def interp_surgery(net, layers):
         filt = upsample_filt(h)
         net.params[l][0].data[range(m), range(k), :, :] = filt
 
-# base net -- follow the editing model parameters example to make
-# a fully convolutional VGG16 net.
-# http://nbviewer.ipython.org/github/BVLC/caffe/blob/master/examples/net_surgery.ipynb
-base_weights = '5stage-vgg.caffemodel'
+def main():
+    max_time_seconds = float(os.environ['TIMEOUT_SECONDS'])
+    start_time = time.time()
+    # base net -- follow the editing model parameters example to make
+    # a fully convolutional VGG16 net.
+    # http://nbviewer.ipython.org/github/BVLC/caffe/blob/master/examples/net_surgery.ipynb
+    base_weights = '5stage-vgg.caffemodel'
 
-# init
-caffe.set_mode_gpu()
-caffe.set_device(0)
+    # init
+    caffe.set_mode_gpu()
+    caffe.set_device(0)
 
-solver = caffe.SGDSolver('solver.prototxt')
+    solver = caffe.SGDSolver('solver.prototxt')
 
-solver_state_paths = glob.glob('snapshot_iter*.solverstate')
-if len(solver_state_paths) > 0:
-    # copy base weights for fine-tuning
-    start_number_idx = 'snapshot_iter_@'.find('@')
-    last_snapshot_number = max(map(lambda path: int(path[start_number_idx:path.find('.')]), solver_state_paths))
-    solver.restore('snapshot_iter_' + str(last_snapshot_number) + '.solverstate')
-    solver.net.copy_from('snapshot_iter_' + str(last_snapshot_number) + '.caffemodel')
-else:
-    solver.net.copy_from(base_weights)
-    
-# do net surgery to set the deconvolution weights for bilinear interpolation
-interp_layers = [k for k in solver.net.params.keys() if 'up' in k]
-interp_surgery(solver.net, interp_layers)
+    solver_state_paths = glob.glob('snapshot_iter*.solverstate')
+    if len(solver_state_paths) > 0:
+        # copy base weights for fine-tuning
+        start_number_idx = 'snapshot_iter_@'.find('@')
+        last_snapshot_number = max(map(lambda path: int(path[start_number_idx:path.find('.')]), solver_state_paths))
+        solver.restore('snapshot_iter_' + str(last_snapshot_number) + '.solverstate')
+        solver.net.copy_from('snapshot_iter_' + str(last_snapshot_number) + '.caffemodel')
+    else:
+        solver.net.copy_from(base_weights)
+
+    # do net surgery to set the deconvolution weights for bilinear interpolation
+    interp_layers = [k for k in solver.net.params.keys() if 'up' in k]
+    interp_surgery(solver.net, interp_layers)
 
 
-# solve straight through -- a better approach is to define a solving loop to
-# 1. take SGD steps
-# 2. score the model by the test net `solver.test_nets[0]`
-# 3. repeat until satisfied
-max_nsteps = 100000
-nsteps = 0
-step_interval = 75
-solver.test_nets[0].share_with(solver.net)
-while nsteps < max_nsteps and (time.time() - start_time <= max_time_seconds):
-    solver.step(step_interval)
-    print 'Running forward on val data'
-    solver.test_nets[0].forward()
-    for i in range(1, 6):
-        print 'My dsn', i, 'loss', solver.test_nets[0].blobs['dsn' + str(i) + '_loss'].data
-    print 'My fuse_loss', solver.test_nets[0].blobs['fuse_loss'].data
-    nsteps += step_interval
-    print 'Completed', nsteps, ', elapsed time (s):', time.time() - start_time
-print 'Completed', nsteps
-print 'Step interval', step_interval
+    # solve straight through -- a better approach is to define a solving loop to
+    # 1. take SGD steps
+    # 2. score the model by the test net `solver.test_nets[0]`
+    # 3. repeat until satisfied
+    max_nsteps = 100000
+    nsteps = 0
+    step_interval = 75
+    solver.test_nets[0].share_with(solver.net)
+    while nsteps < max_nsteps and (time.time() - start_time <= max_time_seconds):
+        solver.step(step_interval)
+        print 'Running forward on val data'
+        solver.test_nets[0].forward()
+        for i in range(1, 6):
+            print 'My dsn', i, 'loss', solver.test_nets[0].blobs['dsn' + str(i) + '_loss'].data
+        print 'My fuse_loss', solver.test_nets[0].blobs['fuse_loss'].data
+        nsteps += step_interval
+        print 'Completed', nsteps, ', elapsed time (s):', time.time() - start_time
+    print 'Completed', nsteps
+    print 'Step interval', step_interval
+
+if __name__ == '__main__':
+    main()
